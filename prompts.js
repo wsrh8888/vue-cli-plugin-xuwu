c4b4212d4e30d4927c896ecfbb0028669226a1cd
renhao1 <renhao1@100tal.com>
2021-04-28 11:29:28 +0800

    [fixed]增加消息提示框

--- prompts.js
+++ prompts.js
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-04-26 16:32:03
- * @LastEditTime: 2021-04-26 16:43:35
+ * @LastEditTime: 2021-04-28 11:28:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue-cli-plugin-init-structure/prompts.js
  */
-module.exports = pkg => {
+module.exports = (pkg, name) => {
   const prompts = [
     {
       type: 'list', 
@@ -17,8 +17,29 @@ module.exports = pkg => {
         { name: 'mobile', value: 'mobile' }
       ],
       default: 'pc',
+    },
+    {
+      type: 'list', 
+      name: 'configType', 
+      message: '请选择使用的场景', 
+      choices: [
+        { name: 'default(默认配置)', value: 'default' },
+        { name: ' Manually select features', value: 'manually' }
+      ],
+      default: 'default',
+    },
+    {
+      when: answers => answers.configType === 'manually',
+      type: 'checkbox', 
+      name: 'manuallyValue', 
+      message: 'Check the features needed for your project', 
+      choices: [
+        { name: 'commit格式化代码', value: 'commit' },
+        { name: 'consoleLog控制台', value: 'consoleLog' },
+        { name: '适配插件', value: 'flexible' }
+      ],
+      default: answers => answers.useType === 'pc' ? ['commit'] : ['commit','consoleLog', 'flexible'],
     }
   ]
-
   return prompts
 }
