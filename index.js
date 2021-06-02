a76ce9e92e5caa050a4437c1d45449cb122f8b72
renhao1 <renhao1@100tal.com>
2021-06-02 14:47:24 +0800

    [fixed]测试

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-04-22 16:33:38
- * @LastEditTime: 2021-04-29 15:09:37
+ * @LastEditTime: 2021-06-02 14:47:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue-plagin/plagin/index.js
  */
 
+dasdceas
 module.exports = (api, opts) => {
 }
