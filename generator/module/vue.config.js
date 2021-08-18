10592187eac476bb5aa67d0b7683ac60a0fb1307
renhao1 <renhao1@100tal.com>
2021-08-18 10:56:31 +0800

    [fixed]修复,配置vueConfig在非线上环境下打包生成map文件用于调试

--- generator/module/vue.config.js
+++ generator/module/vue.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-04-22 17:17:59
- * @LastEditTime: 2021-08-06 11:04:19
+ * @LastEditTime: 2021-08-18 10:49:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue-cli-plugin-structure/generator/template/vue.config.js
@@ -41,7 +41,7 @@ module.exports = {
       })
     ]
   },
-  productionSourceMap: false,
+  productionSourceMap: process.env.NODE_ENV === 'production' && process.env.API_ENV === 'test',
   //lintOnSave 设置为 true 或 'warning' 时，eslint-loader 会将 lint 错误输出为编译警告。默认情况下，警告仅仅会被输出到命令行，且不会使得编译失败。
   //lintOnSave 如果你希望让 lint 错误在开发时直接显示在浏览器中，你可以使用 lintOnSave: 'default'。这会强制 eslint-loader 将 lint 错误输出为编译错误，同时也意味着 lint 错误将会导致编译失败。
   lintOnSave: true,
