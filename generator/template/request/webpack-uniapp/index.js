74c3216ed8aab1236b9289f7e4b093921c31d02d
renhao1 <renhao1@tal.com>
2021-12-10 18:16:39 +0800

    [fixed]删除无用js

--- generator/template/request/webpack-uniapp/index.js
+++
@@ -1,8 +0,0 @@
-import { request } from '@/utils/request'
-import { test } from '@/utils/config'
-export const testApi = () => {
-  return request({
-    method: 'get',
-    url: `${test}/test`
-  })
-}
--- generator/template/utils/config.js
+++
@@ -1,9 +0,0 @@
-let test
-if (process.env.NODE_ENV === 'development' || process.env.API_ENV === 'test') {
-  test = '222'
-} else if (process.env.API_ENV === 'pre') {
-  test = '222'
-} else if (process.env.API_ENV === 'prod') {
-  test = '222'
-}
-export { test }
