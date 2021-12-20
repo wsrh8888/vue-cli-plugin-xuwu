bb61ebe1ff0a6a69f233ea546a1c886fa0179a63
renhao1 <renhao1@tal.com>
2021-12-20 10:59:06 +0800

    [fixed]eslint配置文件中去掉了jsxBracketSameLine选项，被废弃了

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,6 @@ module.exports = {
         // true: <div
         //          className=""
         //          style={{}} >
-        jsxBracketSameLine: false,
         // 箭头函数参数括号 默认avoid 可选 avoid| always
         // avoid 能省略括号的时候就省略 例如x => x
         // always 总是有括号
