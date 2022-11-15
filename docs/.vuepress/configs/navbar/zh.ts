6b833ed98efa6ecb120758eb98f38297020d5cfa
renhao1 <renhao1@tal.com>
2022-11-15 11:00:33 +0800

    [feat]删除无用的文件

--- docs/.vuepress/configs/navbar/zh.ts
+++ docs/.vuepress/configs/navbar/zh.ts
@@ -5,5 +5,16 @@ export const navbarZh: NavbarConfig = [
   {
     text: '指南',
     link: '/zh/guide/getting-started.html',
-  }
+  },
+  {
+    text: '了解更多',
+    children: [
+      {
+        text: '深入Xuwu',
+        children: [
+          '/zh/advanced/architecture.md'
+        ],
+      }
+    ],
+  },
 ]
